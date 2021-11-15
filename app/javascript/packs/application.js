191af18666a0ab7b8b48a4d8e28a2b7dc82c7666
Elton Santos <eltonaxl@hotmail.com>
2021-11-15 04:17:44 -0300

    Adicionando e configurando bootstrap no projeto

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,12 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
+import Rails from "@rails/ujs"
+import "bootstrap"
 import "channels"
+import Turbolinks from "turbolinks"
+import "../stylesheets/application"
 
 import "@fortawesome/fontawesome-free/js/all";
 import "@fortawesome/fontawesome-free/css/all";
